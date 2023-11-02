3224223b08beee7bde752a146f93af168516234a
ajpienaar <ajpienaar123@gmail.com>
2023-11-02 13:21:05 +1300

    majot changes and updates

---
+++ app.js
@@ -0,0 +1,102 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+
+//gets the wind direction from the degrees
+function getWindDirection(degrees) {
+  const directions = ['North', 'North East', 'East', 'South East', 'South', 'South West', 'West', 'North West'];
+  const index = Math.round(((degrees % 360) / 45)) % 8;
+  return directions[index];
+}
+
+const API_KEY = "";//your API key here
+const WEATHER_API_URL = `https://api.openweathermap.org/data/2.5/weather?`;
+
+//handles the API call and displays the data
+async function getWeather(lat, long) {
+  try {
+    const url = `${WEATHER_API_URL}lat=${lat}&lon=${long}&appid=${API_KEY}`;
+    const response = await fetch(url);
+    if(!response.ok){
+        const errorData = await response.json();
+        const errorMessage = errorData.message || `Status: ${response.status}`;
+        throw new Error(errorMessage);
+    }
+    else{
+    const weatherData = await response.json();
+    
+    const city = weatherData.name;
+    const temp = (weatherData.main.temp - 273.15).toFixed(2);
+    const hum = weatherData.main.humidity;
+    const gen = weatherData.weather[0].description;
+    const wind = (weatherData.wind.speed * 3.6).toFixed(2);
+    const direct = weatherData.wind.deg;
+    const iconCode = weatherData.weather[0].icon;
+    const overview = weatherData.weather[0].main;
+
+    let backgroundElement = document.getElementById('background');
+    backgroundElement.className = '';
+
+    if (overview === 'Clouds') {
+        backgroundElement.classList.add("clouds");
+      } else if (overview === 'Clear') {
+        backgroundElement.classList.add("clear");
+      } else if (overview === 'Rain') {
+        backgroundElement.classList.add("rain");
+      } else if (overview === 'Snow') {
+        backgroundElement.classList.add("snow");
+      } else if (overview === 'Thunderstorm') {
+        backgroundElement.classList.add("thunder");
+      } else if (overview === 'Drizzle') {
+        backgroundElement.classList.add("drizzly");
+      } else if (overview === 'Mist' || overview === 'Smoke' || overview === 'Haze' || overview === 'Dust' || overview === 'Fog' || overview === 'Sand' || overview === 'Ash') {
+        backgroundElement.classList.add("foggy");
+      }
+
+    document.getElementById("name").innerText = city + " Weather";
+    document.getElementById("wind").innerText = "Wind: " + wind + " km/h " + getWindDirection(direct);
+    document.getElementById("temp").innerText= "Temp: " + temp + " °C";
+    document.getElementById("humid").innerText= "Humidity: " + hum + " %";
+    document.getElementById("general").innerText= "Overview: " + gen;
+    document.getElementById("weatherIcon").src = `https://openweathermap.org/img/w/${iconCode}.png`;            
+    document.getElementById("overview").innerText = overview;
+  }
+  } catch (error) {
+    console.error("Error fetching weather data:", error);
+    document.getElementById("error").innerText = `Failed to fetch weather: ${error}`;
+  }
+
+}
+document.getElementById("locationForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const selectedLocation = document.getElementById("city").value;
+
+  if (selectedLocation === "Wanaka") {
+    getWeather(-44.70, 169.15);
+  } else if (selectedLocation === "Queenstown") {
+    getWeather(-45.03, 168.66);
+  } else if (selectedLocation === "New York City") {
+    getWeather(40.7128, -74.0060);
+  } else if (selectedLocation === "London") {
+    getWeather(51.5074, -0.1278);
+  } else if (selectedLocation === "Tokyo") {
+    getWeather(35.6895, 139.6917);
+  } else if (selectedLocation === "Sydney") {
+    getWeather(-33.8688, 151.2093);
+  } else if (selectedLocation === "Paris") {
+    getWeather(48.8566, 2.3522);
+  } else if (selectedLocation === "Cape Town") {
+    getWeather(-33.9249, 18.4241);
+  } else if (selectedLocation === "Rio de Janeiro") {
+    getWeather(-22.9068, -43.1729);
+  } else if (selectedLocation === "Moscow") {
+    getWeather(55.7558, 37.6173);
+  } else if (selectedLocation === "Mumbai") {
+    getWeather(19.0760, 72.8777);
+  } else if (selectedLocation === "Beijing") {
+    getWeather(39.9042, 116.4074);
+  } else {
+    console.log("Please select a valid location!");
+  }
+});
+});
